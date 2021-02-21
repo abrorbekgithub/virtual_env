64be098fe1e463ae175d7b0338d493edf79586d0
abrorbek <abrorbekgithub@gmail.com>
2021-02-21 12:02:02 +0500

    query

--- query.py
+++ query.py
@@ -2,9 +2,16 @@ import requests
 import json
 from tinydb import TinyDB,Query
 
-db=TinyDB('db.json')
-qidir=Query()
-a=db.search(qidir.young>10)
+db=TinyDB('user.json')
+user=Query()
+a=db.search(user.dob.age>40)
 for i in a:
-    print(i["name"])
+    print(i["name"]["first"])
 
+# db=TinyDB('user.json')
+# print(db)
+
+# qidir=Query()
+# result=db.search(qidir.age>30)
+
+# print(result)
